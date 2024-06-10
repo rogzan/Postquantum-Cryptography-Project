5351cfd7213eae9fa2af61bd2b48f1e20d645597
rogzan <roksana.ciesla00@gmail.com>
2024-06-10 17:10:25 +0200

    common.py

--- falcon/common.py
+++ falcon/common.py
@@ -1,32 +1,17 @@
-"""This file contains methods and objects which are reused through multiple files."""
+"""Metody i obiekty wielokrotnego użytku używane w różnych plikach."""
 
 
-"""q is the integer modulus which is used in Falcon."""
-q = 12 * 1024 + 1
+q = 12 * 1024 + 1 # q jest stała modulo, która jest używana w Falconie
 
-
+# Podział wielomianu na dwa wielomiany
 def split(f):
-    """Split a polynomial f in two polynomials.
-
-    Args:
-        f: a polynomial
-
-    Format: coefficient
-    """
     n = len(f)
     f0 = [f[2 * i + 0] for i in range(n // 2)]
     f1 = [f[2 * i + 1] for i in range(n // 2)]
     return [f0, f1]
 
-
+# Scalanie dwóch wielomianów w jeden
 def merge(f_list):
-    """Merge two polynomials into a single polynomial f.
-
-    Args:
-        f_list: a list of polynomials
-
-    Format: coefficient
-    """
     f0, f1 = f_list
     n = 2 * len(f0)
     f = [0] * n
@@ -35,9 +20,8 @@ def merge(f_list):
         f[2 * i + 1] = f1[i]
     return f
 
-
+# Kwadratowa norma euklidesowa wektora v
 def sqnorm(v):
-    """Compute the square euclidean norm of the vector v."""
     res = 0
     for elt in v:
         for coef in elt:
